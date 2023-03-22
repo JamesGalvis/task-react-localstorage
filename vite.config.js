ef53429b6e481c827378dc0381fa7f7a7e52a282
JamesGalvis <jamesrgal@gmail.com>
2023-03-21 21:52:42 -0500

    first version

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://jamesgalvis.github.io/TaskApp-React-LocalStorage/",
   plugins: [react()],
 })
